aedfbb104c076160f3bb7b95c24d172f45f2bdd2
Deepika <deepika@shippable.com>
2017-10-12 12:24:30 +0530

    Update add_ssh_key.sh

--- add_ssh_key.sh
+++ add_ssh_key.sh
@@ -1,1 +1,23 @@
 
+export RES_GH_SSH="deep-gh-ssh"
+export RES_GH_SSH_UP=$(echo $RES_GH_SSH | awk '{print toupper($0)}')
+export RES_GH_SSH_META=$(eval echo "$"$RES_GH_SSH_UP"_META")
+
+add_ssh_key() {
+ pushd "$RES_GH_SSH_META"
+ echo "Extracting SSH Key"
+ echo "-----------------------------------"
+ cat "integration.json"  | jq -r '.privateKey' > gh_ssh.key
+ chmod 600 gh_ssh.key
+ ssh-add gh_ssh.key
+ echo "Completed Extracting SSH key"
+ echo "-----------------------------------"
+ popd
+}
+
+main() {
+  add_ssh_key
+  ssh ubuntu@ec2-54-89-188-192 'ls -al'
+}
+
+main
